40fb1d1659e5e6b0ba20ad33a0b76038d486e9b7
XYPB <yuexi.du@yale.edu>
2025-05-21 00:19:32 -0400

    resume

--- scripts/train_stage2.sh
+++ scripts/train_stage2.sh
@@ -29,4 +29,5 @@ deepspeed --include=localhost:0,1 --master_port=65001 train_ds_medplib.py \
   --lr 0.0001 \
   --no_eval \
   --save_steps 400 \
+  --auto_resume
   2>&1|tee -a runs/$exp_name/$time.log
