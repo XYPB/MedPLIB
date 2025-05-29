7fc2968805c5f79082ea4836a2769aef3e8990ea
duyxxd <duyxxd@umich.edu>
2025-05-29 19:18:18 +0300

    dev

--- calc_metrics.py
+++ calc_metrics.py
@@ -64,8 +64,8 @@ def evaluate(test_dict_lst, args, dataset):
                 dataset = 'VQA-RAD-open'
 
         if 'PVQA' in dataset:
-            print('dataset', dataset)
             print(gt_value in ['yes', 'no'])
+            print(gt_value, gt_value == 'yes', gt_value == 'no')
             if gt_value in ['yes', 'no']:
                 eval_closed = True
                 dataset = 'PVQA-yesno'
