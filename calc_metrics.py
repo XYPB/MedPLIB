8859e2a54adec61826d9bfaa9487fba6187ce81e
duyxxd <duyxxd@umich.edu>
2025-05-29 14:36:57 +0300

    dev

--- calc_metrics.py
+++ calc_metrics.py
@@ -72,10 +72,9 @@ def evaluate(test_dict_lst, args, dataset):
             #     hit = 0.0
             # open_hit_scores['hit'].append(hit)
 
-            print('##open', '##pred_value', pred_value, '##gt_value', gt_value)
+            # print('##open', '##pred_value', pred_value, '##gt_value', gt_value, 'score', score)
 
             score = calculate_appearance_with_normalization(pred_value, gt_value, candidate_set)
-            print(score)
 
             open_hit_scores['hit'].append(score)
             open_hit_scores['q_id'].append(item['id'])
