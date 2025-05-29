bff66a1861431145e007d72e46f8050b2472abed
duyxxd <duyxxd@umich.edu>
2025-05-29 14:13:47 +0300

    dev

--- calc_metrics.py
+++ calc_metrics.py
@@ -68,7 +68,7 @@ def evaluate(test_dict_lst, args, dataset):
             #     hit = 0.0
             # open_hit_scores['hit'].append(hit)
 
-            print('##pred_value', pred_value, '##gt_value', gt_value)
+            print('##open', '##pred_value', pred_value, '##gt_value', gt_value)
             print(calculate_appearance_with_normalization(pred_value, gt_value, candidate_set))
 
             open_hit_scores['hit'].append(calculate_appearance_with_normalization(pred_value, gt_value, candidate_set))
@@ -130,7 +130,7 @@ def evaluate(test_dict_lst, args, dataset):
             if gt_value == pred_value_filtered or gt_option in pred_value:
                 closed_scores['hit'].append(1)
             else:
-                print('##pred_value', pred_value, '##gt_value', gt_value)
+                print('##close', '##pred_value', pred_value, '##gt_value', gt_value)
                 closed_scores['hit'].append(0)
             closed_cnt += 1
     
