c00e59ef87269e74850bbc5d4fa7e30f3df2ae13
duyxxd <duyxxd@umich.edu>
2025-05-31 10:10:14 +0300

    merge

---
+++ scripts/merge_stage2_model.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Merge LoRA weights and save the Hugging Face model
+
+python merge_lora_weights_and_save_hf_model.py --version "microsoft/llava-med-v1.5-mistral-7b"  --vision_pretrained "/home/yd344/palmer_scratch/huggingface_models/sam-med2d_b.pth"  --weight runs/medplib-7b-stage2_all/last_ckpt_model/global_step9261/mp_rank_00_model_states.pt --save_path runs/medplib-7b-stage2_all/hf
