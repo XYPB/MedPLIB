41d9c22440f32888d85575db1085fa133a0a16c4
duyxxd <duyxxd@umich.edu>
2025-05-31 10:21:00 +0300

    stage 3 4 gpu

--- scripts/train_stage3.sh
+++ scripts/train_stage3.sh
@@ -5,8 +5,8 @@ exp_name="medplib-7b-stage3"
 exp_dir="runs/$exp_name"
 mkdir -p "$exp_dir"
 
-deepspeed --include=localhost:0,1 --master_port=65000 train_ds_medplib.py \
-  --version="/home/yd344/project/MedPLIB/runs/medplib-7b-stage2/hf" \
+deepspeed --include=localhost:0,1,2,3 --master_port=65000 train_ds_medplib.py \
+  --version="/home/yd344/project/MedPLIB/runs/medplib-7b-stage2_all/hf" \
   --vision_tower='openai/clip-vit-large-patch14-336' \
   --data_path='/home/yd344/dvornek_10t/Datasets/MeCoVQA/train/MeCoVQA-Grounding.json' \
   --val_data_path='/home/yd344/dvornek_10t/Datasets/MeCoVQA/test/MeCoVQA_Grounding_test.json' \
